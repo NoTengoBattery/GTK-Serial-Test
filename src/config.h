9a91dc18fa44594af33b0c0b5fcc61484794c3ea
Oever González <notengobattery@gmail.com>
2018-03-25 18:00:43 -0600

    [GTK] Agrega partes de la GUI -> Agrega 16 labels -> Agrega 16 switches -> Agrega 2 text box -> Agrega 1 botón

---
+++ src/config.h
@@ -0,0 +1,28 @@
+//===-- src/config.h - Definiciones de configuración  -----------------------------------------------------*- C -*-===//
+//
+// Copyright (c) 2018 Oever González
+//
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+//                                 the License. You may obtain a copy of the License at
+//
+//                                      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software  distributed under the License is distributed on
+//  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+//                    specific language governing permissions and limitations under the License.
+//
+//===---------------------------------------------------------------------------------------------------------------===//
+///
+/// Contiene ciertas macros y definiciones de configuración para este proyecto.
+///
+//===---------------------------------------------------------------------------------------------------------------===//
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define APP_ID          "com.notengo.battery.gtk_serial"
+#define APP_MAIN_TITLE  "GTK Serial"
+#define APP_SWI_SIZE    8
+#define APP_SWO_SIZE    8
+
+#endif // CONFIG_H
--- src/main.c
+++ src/main.c
@@ -1,26 +1,78 @@
+#include "config.h"
 #include <gtk/gtk.h>
-static void print_hello(GtkWidget *widget, gpointer data) { g_print("Hello World\n"); }
+
+GtkWidget *input_swi[APP_SWI_SIZE];
+GtkWidget *output_swo[APP_SWO_SIZE];
+GtkWidget *hex_tbi;
+GtkWidget *hex_tbo;
+GtkWidget *send_bto;
+
 static void activate(GtkApplication *app, gpointer user_data) {
+  // Crea una nueva ventana
   GtkWidget *window;
-  GtkWidget *button;
-  GtkWidget *button_box;
   window = gtk_application_window_new(app);
-  gtk_window_set_title(GTK_WINDOW (window), "Window");
-  gtk_window_set_default_size(GTK_WINDOW (window), 200, 200);
-  button_box = gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
-  gtk_container_add(GTK_CONTAINER (window), button_box);
-  button = gtk_button_new_with_label("Hello World");
-  g_signal_connect (button, "clicked", G_CALLBACK(print_hello), NULL);
-  g_signal_connect_swapped (button, "clicked", G_CALLBACK(gtk_widget_destroy), window);
-  gtk_container_add(GTK_CONTAINER (button_box), button);
+  gtk_window_set_title(GTK_WINDOW(window), APP_MAIN_TITLE);
+  // Configura el tamaño inicial de la ventana
+  gtk_window_set_default_size(GTK_WINDOW(window), 100, 100);
+
+  // Usar grid como Layout Manager
+  GtkWidget *grid;
+  grid = gtk_grid_new();
+  // Agrega el grid a la ventana
+  gtk_container_add(GTK_CONTAINER(window), grid);
+
+  // Crea 8 switches para entrada
+  for (int i = 0; i < APP_SWI_SIZE; i++) {
+    GtkWidget *curr_swi = input_swi[i] = gtk_switch_new();
+    // Agrega cada uno de los switches al grid
+    gtk_grid_attach(GTK_GRID(grid), curr_swi, 1, i, 1, 1);
+    // Crear un label para el índice del bit
+    char str[21];
+    sprintf(str, "%d", i);
+    GtkWidget *lbl = gtk_label_new(str);
+    gtk_grid_attach(GTK_GRID(grid), lbl, 0, i, 1, 1);
+  }
+  // 8 para salida
+  for (int j = 0; j < APP_SWO_SIZE; j++) {
+    GtkWidget *curr_swi = output_swo[j] = gtk_switch_new();
+    // Agrega cada uno de los switches al grid
+    gtk_grid_attach(GTK_GRID(grid), curr_swi, 3, j, 1, 1);
+    // Deshabilita esta columna de switches
+    gtk_widget_set_sensitive(GTK_WIDGET(curr_swi), FALSE);
+    // Crear un label para el índice del bit
+    char str[21];
+    sprintf(str, "%d", j);
+    GtkWidget *lbl = gtk_label_new(str);
+    gtk_grid_attach(GTK_GRID(grid), lbl, 2, j, 1, 1);
+  }
+
+  // Crea un textbox para cada columna y un botón para enviar en la columna izquierda
+  hex_tbi = gtk_entry_new();
+  gtk_grid_attach(GTK_GRID(grid), hex_tbi, 0, APP_SWO_SIZE, 2, 1);
+  hex_tbo = gtk_entry_new();
+  gtk_grid_attach(GTK_GRID(grid), hex_tbo, 2, APP_SWO_SIZE, 2, 1);
+  gtk_widget_set_sensitive(GTK_WIDGET(hex_tbo), FALSE); // deshabilita este textbox
+  send_bto = gtk_button_new();
+  gtk_grid_attach(GTK_GRID(grid), send_bto, 0, APP_SWO_SIZE + 1, 4, 1);
+  gtk_button_set_label(GTK_BUTTON(send_bto), "Enviar byte");
+
+  // Muestra la ventana
   gtk_widget_show_all(window);
 }
+
 int main(int argc, char **argv) {
+  // Crea una nueva aplicación de GTK
   GtkApplication *app;
+  // Esta variable almacena el estado de retorno de la aplicación
   int status;
-  app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+  // Devuelve una nueva instancia de la App de GTK
+  app = gtk_application_new(APP_ID, G_APPLICATION_FLAGS_NONE);
+  // Conecta la aplicación a la señal `activate`, con el callback a la función `activate`, sin datos para pasar
   g_signal_connect (app, "activate", G_CALLBACK(activate), NULL);
+  // Lanza la aplicación `app` de GTK, con los argumentos argc, argv y bloquea hasta que la aplicación termina
   status = g_application_run(G_APPLICATION (app), argc, argv);
+  // Libera la instancia de la `app` (liberando memoria)
   g_object_unref(app);
+  // Retorna
   return status;
 }
